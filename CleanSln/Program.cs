cf4a0685795c17561b8f36903f86a7638caa444f
Nils Feierabend <mztikk@outlook.de>
2021-12-13 20:13:22 +0100

    Add project files.

---
+++ CleanSln/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using Microsoft.Build.Construction;
+
+[DllImport("msvcrt.dll")]
+static extern int system(string format);
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No solution file provided");
+    return;
+}
+
+string sln = args[0];
+if (string.IsNullOrEmpty(sln))
+{
+    Console.WriteLine("No solution file provided");
+    return;
+}
+
+if (Path.GetExtension(sln) != ".sln")
+{
+    Console.WriteLine($"'{sln}' not a valid solution file");
+    return;
+}
+
+SolutionFile solution = SolutionFile.Parse(sln);
+foreach (SolutionConfigurationInSolution? config in solution.SolutionConfigurations)
+{
+    system($"msbuild -t:Clean -p:Configuration={config.ConfigurationName} -p:Platform={config.PlatformName} {sln}");
+}
